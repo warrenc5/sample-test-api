b94e79688e0f8ce8355e55fe7a741f1278d83f2c
Warren Crossing <warren.crossing@juno-software.com>
2019-01-21 22:51:43 +1100

    add account creater api

--- src/main/java/devnull/mnf/resource/api/AccountLocal.java
+++ src/main/java/devnull/mnf/resource/api/AccountLocal.java
@@ -3,7 +3,10 @@ package devnull.mnf.resource.api;
 import devnull.mnf.model.Account;
 import java.util.Collection;
 import javax.ejb.Local;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -16,4 +19,10 @@ public interface AccountLocal {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     Collection<Account> retrieveAll() throws Exception;
+
+    @Path("/")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    void retrieveAll(@NotNull Account account) throws Exception;
 }
